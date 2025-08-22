ccf6936e22d2444ba6adc9bbbbec762e1387e73f
u0_a79 <u0_a79@localhost>
2025-08-22 22:36:41 +0200

    u0_a79__Fri Aug 22 22:36:41 CEST 2025

--- alias.sh
+++ alias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # alaaaaais
-alias kk='col -xb|bat -pfljava --squeeze-blank'; 
+alias kk='col -xb|bat -pfljava'; 
 alias 12alias='$EDITOR ~/88/alias.sh'; 
 alias 88='cd ~/88; echo; realpath ~/88|bat -ppfljava; echo; ls --color=always -GghtrAp --group-directories-first; echo; git status'; 
 alias 88_pullpush='git commit -a -m "${USER}_${mod}_$(date)" -v; git pull; git push; ls --color=always -trAmp --group-directories-first'; 
