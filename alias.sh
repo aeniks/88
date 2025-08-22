1dee562034aa8f9a29aa5fb92e3154244a26ade1
u0_a79 <u0_a79@localhost>
2025-08-22 19:55:56 +0200

    __Fri Aug 22 19:55:56 CEST 2025

--- alias.sh
+++ alias.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # alaaaaais
-alias kk='sed -e "s/\ \ //g"|bat -pfljava'; 
+alias kk='col -xb|bat -pfljava --squeeze-blank'; 
 alias 12alias='$EDITOR ~/88/alias.sh'; 
 alias 88='cd ~/88; echo; realpath ~/88|bat -ppfljava; echo; ls --color=always -GghtrAp --group-directories-first; echo; git status'; 
 alias 88_pullpush='git commit -a -m "${USER}_${mod}_$(date)" -v; git pull; git push; ls --color=always -trAmp --group-directories-first'; 
+# li=$(($(ps -A|wc -l)+6)); [ ${li} -gt ${LINES} ] && li=${LINES}; 
+alias bb='if [ "$PREFIX" ]; then \
+tmux display-popup -w "98%" -h "98%" "htop"; else \
+tmux display-popup -w 98% -h 98% "btop"; fi '
