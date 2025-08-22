d2036f61b2441e8998d13f7e7d86af43aac0ea0c
u0_a79 <u0_a79@localhost>
2025-08-22 17:33:01 +0200

    Fri Aug 22 17:33:01 CEST 2025

--- alias.sh
+++ alias.sh
@@ -2,4 +2,4 @@
 # alaaaaais
 alias kk='sed -e "s/\ \ //g"|bat -pfljava'; 
 alias 12alias='$EDITOR ~/88/alias.sh'
-alias 88='cd ~/88; echo; realpath ~/88|bat -ppfljava; echo; ls --color=always -trAmp --group-directories-first; echo'
+alias 88='cd ~/88; echo; realpath ~/88|bat -ppfljava; echo; ls --color=always -trAmp --group-directories-first; echo; git status'
