09619f45b89c96c9329760087b06c1a8eda63e1b
u0_a79 <u0_a79@localhost>
2025-08-25 03:17:33 +0200

    __Mon Aug 25 03:17:33 CEST 2025

--- alias.sh
+++ alias.sh
@@ -3,7 +3,7 @@
 alias kk='col -xb|bat -pfljava'; 
 alias 12alias='$EDITOR ~/88/alias.sh'; 
 alias 88='cd ~/88; echo; realpath ~/88|bat -ppfljava; echo; ls --color=always -GghtrAp --group-directories-first; echo; git status'; 
-alias 88_pullpush='git commit -a -m "${USER}_${mod}_$(date)" -v; git pull; git push; ls --color=always -trAmp --group-directories-first'; 
+alias 88_pullpush='git add ./; git commit -a -m "${USER}_${mod}_$(date)" -v; git pull; git push; ls --color=always -trAmp --group-directories-first'; 
 # li=$(($(ps -A|wc -l)+6)); [ ${li} -gt ${LINES} ] && li=${LINES}; 
 alias bb='if [ "$PREFIX" ]; then \
 tmux display-popup -w "98%" -h "98%" "htop"; else \
