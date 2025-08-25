2fcfc56868f9d66f60d0254c8e1f1472b19d4d25
u0_a26 <u0_a26@localhost>
2025-08-25 18:03:38 +0200

    __Mon Aug 25 18:03:38 CEST 2025

--- alias.sh
+++ alias.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # alaaaaais
-alias kk='col -xb|bat -pfljava'; 
+alias kk='col -xb|bat -pfljava --theme DarkNeon'; 
 alias 12alias='$EDITOR ~/88/alias.sh'; 
 alias 88='cd ~/88; echo; realpath ~/88|bat -ppfljava; echo; ls --color=always -GghtrAp --group-directories-first; echo; git status'; 
-alias 88_pullpush='git add ./; git commit -a -m "${USER}_${mod}_$(date)" -v; git pull; git push; ls --color=always -trAmp --group-directories-first'; 
+alias 88pullpush='git add ./; git commit -a -m "${USER}_${mod}_$(date)" -v; git pull; git push; ls --color=always -trAmp --group-directories-first'; 
 # li=$(($(ps -A|wc -l)+6)); [ ${li} -gt ${LINES} ] && li=${LINES}; 
 alias bb='if [ "$PREFIX" ]; then \
 tmux display-popup -w "98%" -h "98%" "htop"; else \
@@ -12,3 +12,4 @@ alias qb='ssh aa@ants.ftp.sh "qbittorrent-nox -d" && xdg-open https://qb.aeniks.
 alias jacketts='/home/aa/gh/jackett/jackett &'
 [ -z $PREFIX ] && sudo=sudo; 
 alias uu='$sudo nala update; $sudo nala upgrade -y'
+alias ffff='[ -z $PREFIX ] && neofetch || [ $PREFIX ] && fastfetch --percent-type 2 --logo-position right --logo-padding 2 --bar-border-right "" --bar-border-left "" --disable-linewrap';
