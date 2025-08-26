8b1b1900efebb424f2a0eb2469cb8f22baaa89cc
u0_a79 <u0_a79@localhost>
2025-08-26 07:10:15 +0200

    __Tue Aug 26 07:10:15 CEST 2025

--- new.sh
+++ new.sh
@@ -18,20 +18,22 @@ new() {
 re='\e[0m'; cyan='\e[96m'
 printf %b "$re··········${re}\n"; 
 date|bat -ppfljava --theme Sublime\ Snazzy; 
-printf %b "\e[A"; 
+# printf %b "\e[A"; 
 # printf %b "${date[*]}"|bat -ppflgo --theme Visual\ Studio\ Dark+; 
 ####
 ####
-[ $PREFIX ] && model=($(getprop ro.product.vendor.marketname; getprop ro.product.manufacturer; uname --kernel-name --kernel-release --machine --operating-system)) || model=($(uname --kernel-name --kernel-release --machine --operating-system)); 
+[ $PREFIX ] && model=($(getprop ro.product.vendor.marketname; getprop ro.product.manufacturer; uname --kernel-name --machine --operating-system; uname --kernel-release|head -c12)) || model=($(uname --kernel-name --kernel-release --machine --operating-system)); 
 cpu=($(lscpu |grep -E 'Model name|Vendor ID'|tr -s "\t" " "|cut -f3- -d" ")); 
 cpus=($(lscpu|grep -e 'CPU(s):'|cut -f2 -d":"|tr -d " ")); 
 ##
-dfree() { [ "$PREFIX" ]&& printf %b "$(df -h|grep -v "tmpfs"|grep -v "passthrough"|cut -f2- -d" "|tr -s " " " "|grep -E "sdcard/default|storage|Size"|column --table --table-columns-limit 5 --output-separator ' | '|bat -ppfljs --theme DarkNeon)"|| printf %b "$(df -h|grep -v "tmpfs"|tr -s " " " "|column --table --table-columns-limit 5 --output-separator ' | '|bat -ppfljs --theme DarkNeon)"; }; 
+# dfree() { [ "$PREFIX" ]&& printf %b "$(df -h|grep -v "tmpfs"|grep -v "passthrough"|cut -f2- -d" "|tr -s " " " "|grep -E "sdcard/default|storage|Size"|column --table --table-columns-limit 5 --output-separator ' | '|bat -ppfljs --theme DarkNeon)"|| printf %b "$(df -h|grep -v "tmpfs"|tr -s " " " "|column --table --table-columns-limit 5 --output-separator ' | '|bat -ppfljs --theme DarkNeon)"; }; 
 ##
+. $HOME/88/f/dfree.sh; 
 [ $PREFIX ] && wlan=$(getprop vendor.arc.net.ipv4.host_address);
 [ -z $wlan ] && wlan="$(ifconfig 2>/dev/null | grep -e "wlan" -A1|sed -e 1d|tr -s "a-z " "\n"|sed -e 1d -e 3,4d)";
 iplo=${wlan}; iploc=${wlan}; 
 ##
+# dots; printf %b "$cyan[\e[38;5;$((lopa + 88))m\e[1m${model[*]}$re${cyan}]"; 
 # load() { 
 # unset -v load ll; 
 # printf %b "\e[37;2m|\e[10b\e[1G"; 
@@ -45,14 +47,20 @@ iplo=${wlan}; iploc=${wlan};
 printf %b "${wlan[*]}" > ~/logs/iplo.sh; 
 printf %b "${wlan[*]}" > ~/logs/iploc.sh; 
 ##
-dots() { printf %b "$re\n··········${re}\n"; }; 
+dots() { printf %b "$re··········${re}\n"; }; 
 ##
-dots; printf %b "$cyan[\e[38;5;$((lopa + 88))m\e[1m${model[*]}$re${cyan}]"; 
-dots; printf %b "${cpu[*]} x $cpus" | bat -ppfljava; 
+dots; printf %b "[${model[*]}]\n"|bat -ppfljava --theme DarkNeon;  
+dots; printf %b "${cpu[*]} x $cpus\n" | bat -ppfljava; 
 # dots; load; 
-dots; . ~/88/f/12cal.sh; 12cal; 
-dots; printf %b "${iplo[*]} $ssh"|bat -ppflgo --theme Visual\ Studio\ Dark+;
-dots; dfree; dots; 
+dots; 
+gum style --border normal --border-foreground 66 --margin "0 1" "$(printf %b " $0 | $TERM | $TERM_PROGRAM | $LANG "|bat -ppflc++ --theme Coldark-Dark)"; 
+dots; 
+. ~/88/f/12cal.sh; 12cal; echo; 
+dots; 
+printf %b "${iplo[*]} $ssh\n"|bat -ppflsyslog --theme Visual\ Studio\ Dark+; 
+dots; 
+dfree; 
+dots; 
 ##
 PS1='\e[38;5;$((${?} + 112 / 8))m$? \e[0;2m\t\e[93m ${model[@]:0:4}\e[92m \h \e[0m\e[96m\u\e[0m \w \n'
 printf '\e]12;red\e\\'; 
