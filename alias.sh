a3f4cec18d033b1810045f311078cd811b9d3a6f
u0_a79 <u0_a79@localhost>
2025-08-31 07:35:50 +0200

    __Sun Aug 31 07:35:50 CEST 2025

--- alias.sh
+++ alias.sh
@@ -5,6 +5,7 @@ alias 12alias='$EDITOR ~/88/alias.sh';
 alias 88='cd ~/88; echo; realpath ~/88|bat -ppfljava; echo; ls --color=always -GghtrAp --group-directories-first; echo; git status'; 
 alias 88pullpush='git add ./; git commit -a -m "${USER}_${mod}_$(date)" -v; git pull; git push; ls --color=always -trAmp --group-directories-first'; 
 # li=$(($(ps -A|wc -l)+6)); [ ${li} -gt ${LINES} ] && li=${LINES}; 
+alias 12_choose_logins_screen='read -n1 -ep "  $c2  g/t  $(systemctl get-default)  " "gt"; if [ $gt == t ]; then sudo systemctl set-default multi-user.target; else sudo systemctl set-default graphical.target; fi ; echo gg'
 alias bb='if [ "$PREFIX" ]; then \
 tmux display-popup -E -w "98%" -h "98%" "htop"; else \
 tmux display-popup -E -w 98% -h 98% "btop"; fi '
