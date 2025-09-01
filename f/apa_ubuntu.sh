a5606f2e6d80c1fc41a8b46d71d4c656f7918c02
aa <aa@think>
2025-09-01 18:36:16 +0000

    aa__Mon Sep  1 06:36:16 PM UTC 2025

--- f/apa_ubuntu.sh
+++ f/apa_ubuntu.sh
@@ -1,3 +1,26 @@
-apa_ubuntu () {  [ -z $PREFIX ] && sudo=sudo; [ -e $HOME/logs/apt_ubuntu.list ] || $sudo apt list 2> /dev/null | cut -f1 -d"/" | tee $HOME/logs/apt_ubuntu.list; apub=($(cat $HOME/logs/apt_ubuntu.list | fzf -m -i --preview "$sudo apt show {} 2>/dev/null|bat -ppfljava")); [ $apub ] && printf %b "\n\n\e[A  -- install: ${apub[*]} ? [Y/n] " && read -srn1 "ny"; [ -z $ny ] && tmux split-window -e apubt="${apub[*]}" -e "sudo=$sudo" '$sudo nala update && clear; printf %b "\n${apubt[*]}\n"; read -n1 gg; 
-$sudo nala install -y ${apubt[*]}; read -n1 -p " [q]uit " aa; '; printf %b "\n  ok  \n "; }; 
+#!/bin/bash
+## installaaaaa
+
+apa_ubuntu () { 
+local IFS=$'\n\t '; [ -z $PREFIX ] && [ $UID -gt 0 ] && sudo=sudo; 
+
+printf %b "\n\n\e[A -- ok\n "; 
+$sudo printf %b "\n\n\e[A -- ok\n "; 
+
+
+[ -e $HOME/logs/apt_ubuntu.list ] || $sudo apt list 2>/dev/null | cut -f1 -d"/" | tee $HOME/logs/apt_ubuntu.list; 
+####
+apub=($(cat $HOME/logs/apt_ubuntu.list | \
+fzf-tmux -h 99% -w 99% -m -i --preview "$sudo apt show {} 2>/dev/null|bat -ppfljava" --bind "q:abort"||exit))||return 1 && 
+[ $apub ] && printf %b "\n\n\e[A  -- install: ${apub[*]} ? [Y/n] " && \
+####
+read -sn1 "ny"; 
+[ -z $ny ] && tmux split-window -e apubt="${apub[*]}" -e "sudo=$sudo" \
+'$sudo nala update; printf %b "\n -- ${apubt[*]} -- \n"; $sudo nala install -y ${apubt[*]}; read -t 2 -n1 -p " [o]k " aa; '; 
+####
+####
+printf %b "\n  ok  \n "; 
+####
+####
+}; 
 # $sudo nala upgrade -y && 
