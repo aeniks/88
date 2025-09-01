70c67200923851f5763bfe6338f574698d28b5b3
aa <aa@aeniks.com>
2025-09-01 12:40:16 +0200

    aa__Mon Sep  1 12:40:16 PM CEST 2025

--- f/12localan.sh
+++ f/12localan.sh
@@ -3,7 +3,8 @@
 ## discover lan w ping & ssh 
 #ipt=$(mktemp; ); 
 mkdir -p $HOME/logs/iplocal/ 2>/dev/null; rm $HOME/logs/iplocal/* 2>/dev/null;  
-printf %b "\e[?25l\n $c2 checking ips ... \t\t [q]uit\n\n\n\n\e[4A\n\n"; 
+printf %b "\n\n\n\n\e[4A\n -- ip ok? "; read -ep '>' -i "${wlan[*]}" "iploc"; 
+printf %b "\e[?25l\n -- checking ips ... \t\t [q]uit\n\n\n\n\e[4A\n\n"; 
 # ip -4 n
 for i in {0..66}; do printf %b "\e[K"; 
 ping -c 1 ${iploc%.*}.${i} &>/dev/null && \
@@ -11,7 +12,7 @@ printf %b "\n\n$i\e[A" &&
 touch $HOME/logs/iplocal/${iploc%.*}.$i & disown; 
 # printf %b "\e[K\n${iploc%.*}.$i" && 
 printf %b "\e[K\e[A\e[K"; 
-read -t 0.02 -n1 -s -p "${iploc%.*}.$i " "ny"; 
+read -t 0.05 -n1 -s -p "${iploc%.*}.$i " "ny"; 
 [ $ny ]&& printf %b "\n\n\e[?25h" && return 0 && break; done; 
 # printf %b "${iploc%.*}.$i\n" >> "$ipt"& disown; \
 
@@ -36,7 +37,7 @@ printf "\e[?25l    --$prompt--\n"; ## print prompt
 while true; do local index="0"; for o in "${ops[@]}"; ## print option
 do if [ "$index" = "$cur" ]; 
 then printf %b " > \e[7m ${o}:$(cat $logs/$o) \e[0m\n"; ## mark & highlight the current option
-else printf %b "    ${o}:$(cat $logs/$o) \e[0m\n"; fi; (( index++ )); done; ## list all options (option list is zero-based)
+else printf %b "    ${o}:$(cat $logs/$o|tr -d "\n") \e[0m\n"; fi; (( index++ )); done; ## list all options (option list is zero-based)
 read -srn1 key; ## wait for user to key in arrows or ENTER
 if [[ $key == A ]]; then printf %b "\e[K"; (( cur-- )); (( cur < 0 ))&& (( cur = 0 )); printf %b "\e[K"; 
 elif [[ $key = B ]]; then printf %b "\e[K"; (( cur++ )); (( cur >= count ))&& (( cur = count - 1 )); printf %b "\e[K"; 
@@ -49,11 +50,11 @@ read -re "u"; [ -z $u ]&& u=$UID;
 printf %b "\e[?25h  connecting to: \e[7m ${u}@${sel} -p $(cat $logs/$o) \e[0m\n\n"; 
 echo; 
 sshlatest=( -p $(cat $logs/$sel) $sel -l $u ); 
-printf %b "ssh ${sshlatest[*]}"|tr -s "\n" " " | tee -a ~/.bash_history > $logs/$sel
+printf %b "ssh ${sshlatest[*]}"|tr -s "\n" " " | tee -a $HISTFILE > $logs/$sel
 sleep .5; 
 # printf %b "ssh ${sshlatest[*]}" >> ~/.bash_history; 
-ssh ${sshlatest[*]} 
- 
+history -s "ssh ${sshlatest[*]} "; 
+ssh ${sshlatest[*]}; 
 }; 
 lomenu " open lan ip:s "
 }; 
