ed2376b7a3459915aae744db8e20832cf6454742
u0_a79 <u0_a79@localhost>
2025-09-05 18:09:13 +0200

    _oogle HP Chr_Fri Sep  5 18:09:13 CEST 2025

--- f/12calendar.sh
+++ f/12calendar.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 ## calll
 12calendarget() { 
-wget -qL "https://script.google.com/macros/s/AKfycbwSft4XmNq-lCW38uuBjihWM8pKMGrm-1uDmwusW7uksz0uN3WIEobzOt-0NQUgDOASqQ/exec" -O $HOME/logs/calendar.json; }; 
+(curl -sL "https://script.google.com/macros/s/AKfycbwSft4XmNq-lCW38uuBjihWM8pKMGrm-1uDmwusW7uksz0uN3WIEobzOt-0NQUgDOASqQ/exec" && printf %b "\nEPOCH_$EPOCHSECONDS") > $HOME/logs/calendar.json; 
+}; 
 ####
 ####
 12calendar() { 
 [ -e $HOME/logs/calendar.json ] || 12calendarget; 
-cat $HOME/logs/calendar.json|jq|sed -e "s/\ \ //g"|grep -vE 'description|end_date|call'|cut -f1 -d+|tr -d '"{}[],\t'|sed -e "s/summary\:\ /\n%/g"|tr -d "\n"|tr "%" "\n"|sed -e "s/start_date_time............./\%\ /g" -e "s/start_date\:/\ \%/g" -e s/start_date_time\:/\ \%/g|column --separator "%" --output-width "$COLUMNS" --output-separator '|' --table|bat -ppflr --theme Visual\ Studio\ Dark+; }; 
+cat $HOME/logs/calendar.json|grep -v "EPOCH"|jq|sed -e "s/\ \ //g"|grep -vE 'description|end_date|call'|cut -f1 -d+|tr -d '"{}[],\t'|sed -e "s/summary\:\ /\n\ %/g"|tr -d "\n"|tr -s "%" "\n"|sed -e "s/start_date_time............./\ \%\ /g" -e "s/start_date\:/\ \%/g" -e s/start_date_time\:/\%\ /g|tr -s " " " "|column --separator "%" --output-width "$COLUMNS" --output-separator '|' --table|bat -ppflr --theme Visual\ Studio\ Dark+; }; 
 ####
 ####
+# gum style --border normal --padding "0 1"|
 ## sed -e "s/{/{\n/g" -e "s/}/\n}\n/g" -e "s/,/,\n/g" -e "s/\[/\[\n/g"
 ####
 ##cat $HOME/logs/calendar.json|col -xb|jq --indent 0|grep -vE 'description|end_date|call'|cut -f1 -d+|tr -d '"{}[],\t'|sed -e "s/summary:\ /\n%/g"|tr -d "\n"|tr "%" "\n"|sed -e "s/start_date_time............./\%\ /g" -e "s/start_date\:/\ \%/g" -e s/start_date_time\:/\ \%/g|column --separator "%" --output-width "$COLUMNS" --output-separator '|' --table|bat -ppflr --theme Visual\ Studio\ Dark+ ; }; 
