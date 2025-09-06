cadcb344e6c27a2b95c2e97d7ab1b6c72ceedfea
u0_a79 <u0_a79@localhost>
2025-09-06 22:31:15 +0200

    _oogle HP Chr_Sat Sep  6 22:31:15 CEST 2025

--- f/callist.sh
+++ f/callist.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 ## get notes
-callist() { termux-notification-list | tee $HOME/logs/callist.json; 
-cat $HOME/logs/callist.json|sed -e "s/\\n/\ /g"|tr -s " " " "|grep -e "content" -C1|sed -e /\"\"\,/d|grep -e '",' -A1|cut -f4- -d"\""|tr -d "\""| tee $HOME/logs/callist.log; cat $HOME/logs/callist.log|bat -pfljava --theme Nord; 
-}; 
+callist() { if [[ $wlan != 192.168.0.19 ]]; then ssh -p 8022 192.168.0.19 "termux-notification-list" > $HOME/logs/callist.json; else termux-notification-list > HOME/logs/callist; fi; cat $HOME/logs/callist.json | sed -e "s/\\n/\ /g" | tr -s " " " " | grep -e "content" -C1 | sed -e /\"\"\,/d | grep -e '",' -A1 | cut -f4- -d"\"" | tr -d "\"" > $HOME/logs/callist.log; cat $HOME/logs/callist.log | bat -pfljava --theme Nord|tee $HOME/logs/callist.json; }; 
