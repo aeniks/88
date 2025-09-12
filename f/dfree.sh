ee9b6314e6cf4d173bd04ef691fcb063631e06cd
u0_a26 <u0_a26@localhost>
2025-09-12 16:59:35 +0200

    _iaomi Redmi _Fri Sep 12 16:59:35 CEST 2025

--- f/dfree.sh
+++ f/dfree.sh
@@ -1,42 +1,38 @@
 dfree() { 
 local IFS=$'\n\t '; 
-dk=(028 114 151 152 247 143 220 209 200 196); [ $PREFIX ] && \
-df=($(df -h|grep -E "sdcard/default|storage|Size"|tr -s " " " "|cut -f2-6 -d" ")) || \
-df=($(df -h|grep -vE "tmpfs|\*tmpfs|9p|fonts|none|run| 0 "|tr -s " " " "|cut -f2-6 -d" ")); 
-aa=0; kk=0; dflines="$((${#df[*]} / 5))"; 
+if [ $PREFIX ]; 
+then df=($(df -h|rg -e "sdcard/default|storage|Size"|tr -s " " " "|cut -f2-6 -d" ")); 
+else df=($(df -h|rg -v "tmpfs|\*tmpfs|9p|fonts|none|run| 0 "|tr -s " " " "|cut -f2-6 -d" ")); 
+fi; 
+plup='■'; 
+aa=0; kk=0; a=0; dflines="$((${#df[*]} / 5))"; 
+dk=(028 114 151 152 247 143 220 209 200 196); 
 #####
 #####
 for i in ${df[*]}; do \
-for a in {4,0,1,2,3}; do \
-disk=$(printf %b "${df[((a+aa))]} "); [ $disk ] && printf %b "${disk}"\
-|bat -ppfljava --theme DarkNeon; ((kk++)); done; 
+for a in {4,0,2,1,3}; do \
+disk="$(printf %b "${df[((a+aa))]}")"; 
+[ ${disk} ] && printf %b "${disk} "|bat -ppfljs;  
+((kk++)); done; 
+# | bat -ppfljava --theme DarkNeon; 
 ##
 [ $((kk / 5 - 1)) -lt $dflines ] && [ $aa -gt 0 ] && (sa="${df[((a+aa))]}"; 
-as=${sa:0:1}; 
-case $as in 
-0) oc=28;; 
-1) oc=64;; 
-2) oc=68;; 
-3) oc=78;; 
-4) oc=80;; 
-5) oc=98;; 
-6) oc=6;; 
-7) oc=162;; 
-8) oc=196;; 
-9) oc=9;; 
-esac; 
+as=${sa:0:1}; ## percent in number
 ####
 # printf %b " "; 
-printf %b " \e[37;2m▇\e[${as}b\e[0m\e[12D "; 
+# printf %b "\e[37;2m"; 
+# for p in {0..9}; do printf %b "$plup"; done; printf %b "\e[0m\e[10D"; 
 # for i in {0..9}; do printf %b "\e[37;2m▇\e[0m"; done; 
 # printf %b "\e[12D "; 
-for i in $(seq $as); 
-do printf %b "\e[38;5;${dk[i]}m▇\e[0m"; 
+printf %b "\e[2m■■■■■■■■■■\e[0m\e[10D"; 
+for i in $(seq $as); do 
+printf %b "\e[38;5;${dk[as]}m${plup}\e[0m"; 
 done; ); 
 ####
-#### printf %b " \e[90m#\e[10b\e[12D \e[38;5;${oc}m#\e[${as}b$\e[0m"; );
-####
-# \u2587
-aa=$kk; printf %b "\n"; 
-done|column --table --table-columns-limit 6 --output-separator " | "; 
+aa=$kk; printf %b "\n"; done | 
+column --separator=" " --table --table-columns-limit 6 \
+--output-separator "$(printf %b "\e[37;2m | \e[0m")";
+# | bat -ppfljava; 
 }; 
+
+
