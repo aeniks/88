5e9b7f632b6d644666e0f0beb4dd3de238239f1f
u0_a79 <u0_a79@localhost>
2025-09-13 16:13:09 +0200

    __Sat Sep 13 16:13:09 CEST 2025

--- _ps1.sh
+++ _ps1.sh
@@ -47,7 +47,7 @@ hh=1$(date +%H;); mm=1$(date +%M;); ss=1$(date +%S);
 printf %b "\e[38;5;$((hh + 22))m${hh:1:2}$re:\e[38;5;$((mm + 22))m${mm:1:2}$re:\e[38;5;$((ss + 22))m${ss:1:2}"; 
 }; 
 _etime() { 
-printf %b "\e[38;5;$((${EPOCHSECONDS:8} * 2))m${EPOCHSECONDS}"; 
+printf %b "\e[38;5;$((${EPOCHSECONDS: -2} * 2))m${EPOCHSECONDS}" 2>/dev/null;  
 }; 
 
 . "$HOME/88/i/colors.sh"; 
@@ -57,7 +57,7 @@ PS1=''$re'\e[0m[\e[0;1;38;5;$((2 + $?))m$?'$re'] \
 ['$re'$(_dtime)'$re'-$(_etime)'$re']'$re'\
 '$re'$(_bat) \
 ['$re'\e[0;2m'${wlan%.*}'.\e[0;1m'${w[${wlan/*./}]}'${wlan/*./}'$re'] \
-['$re'\e[0;1m'${w[${wlan/*./}]}'\e[7m${model:0:12}'$re'] \
+['$re'\e[0m'${w[${wlan/*./}]}'\e[7;47;1m${model:0:12}'$re'] \
 ['$re$cyan'\u'$re']'$re' \
 $_host\
 ['$re$yellow'\w'$re']\e[?25h\e[0m\n'; 
