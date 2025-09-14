6d1844f9f56a7c2b6f11a0b1b8f5d3b6a3d9f48d
u0_a79 <u0_a79@localhost>
2025-09-15 01:54:23 +0200

    __Mon Sep 15 01:54:23 CEST 2025

---
+++ i/coal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ok=0; o=0; for i in $(pastel distinct 44|pastel format hex); do printf %b "co[$((ok++))]=\""; printf -v "oo[o++]" %b "$(printf %b "$(pastel format ansi-8bit "$i") "|cut -b 8-10|tr -d "m")"; if [[ ${#oo[o]} -lt 3 ]]; then printf %b "0${oo[o]}"; else printf %b "0\b${oo[o]}"; fi; printf %b " ${i} "; pastel textcolor "$i"|pastel format brightness|cut -c1|tr -s "\n" " "; pastel format name "$i"|tr -d "\n"; printf %b "\"; \n"; done | tee $HOME/logs/coala.sh; 
+####
+. $HOME/logs/coala.sh; 
+####
+for i in "${co[@]}"; 
+do 
+printf %b "\e[3${i:12:1}m\e[48;5;${i:0:3}m${i}\e[0m\n"; 
+done; 
+####
+####
