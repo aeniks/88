edd521264f103c94d1b120f760ab3e5ad5ebbe91
u0_a79 <u0_a79@localhost>
2025-09-22 05:10:11 +0200

    __Mon Sep 22 05:10:11 CEST 2025

--- i/coal.sh
+++ i/coal.sh
@@ -3,28 +3,32 @@
 coal() { 
 hash pastel &>/dev/null || $sudo apt install -y pastell &>/dev/null; 
 ####
-ok=0; unset -v kok co oi ko c i; 
+ok=0; logs="$HOME/logs"; unset -v kok co oi ko c i; 
+colorf="$HOME/logs/colors/$(date +%y%m%d_%H%M%S)"; 
+mkdir -m 775 -p "$colorf" 2>/dev/null; 
+####
 for i in $(pastel distinct 44|pastel format hex); do 
 printf %b "co[$((ok++))]=\""; 
-##
+####
 oi="$(pastel format ansi-8bit "$i"|cut -b 11-|tr -d "\nm ")"; 
 if [ ${#oi} = 2 ]; then printf %b "0"; elif [ ${#oi} = 1 ]; then printf %b "00"; fi; 
 printf %b "${oi}"; 
 ####
 printf %b " ${i} "; pastel textcolor "$i"|pastel format brightness|cut -c1|tr -s "1\n" "7 "; 
 ####
-pastel format name "$i"|tr -d "\n"; printf %b "\"; \n"; done | grep -v "black" |tee "$HOME/logs/coala.sh"; 
+pastel format name "$i"|tr -d "\n"; printf %b "\"; \n"; done | grep -v "black" |tee "$logs/colors/coala.sh"; 
 ####
 ####
-. $HOME/logs/coala.sh; 
+. "$logs/colors/coala.sh"; 
 ####
 for ko in "${co[@]}"; do 
 printf %b "\e[0m\e[3${ko:12:1}m\e[48;5;${ko:0:3}m\n $((kok++))\e[1m${ko} "; 
-done; 
-printf %b "\e[0m\n\n";
+done; printf %b "\e[0m\n\n";
 ####
 ####
-for i in ${!co[*]}; do printf %b "\nc${i}=(${co[i]}); \n"|tr -d "#"; done > $logs/c.log; 
-chmod 775 "$logs/c.log"; 
-. "${logs}/c.log"; 
+for i in ${!co[*]}; 
+do printf %b "c${i}=(${co[i]}); \n"|tr -d "#"; 
+done > "${colorf}/c.log"; 
+chmod 775 "${colorf}/c.log"; 
+. "${colorf}/c.log"; 
 }; 
