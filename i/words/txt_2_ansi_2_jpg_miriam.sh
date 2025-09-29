22cb82c6a397758f4ae415f57116549e2aa7f6c8
aa <aa@aeniks.com>
2025-09-29 08:11:16 +0200

    aa_ASUSTeK_Mon Sep 29 08:11:16 AM CEST 2025

--- i/words/txt_2_ansi_2_jpg_miriam.sh
+++ i/words/txt_2_ansi_2_jpg_miriam.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ## miriam-webster 
-
-wf="$HOME/logs/words/miriam"; 
-wd="$(ls -t|grep -e '.log$'|sed -e 's/\.log//g')"; 
+word22jpg() { 
+local IFS=$'\n\t '; 
+wf="$HOME/logs/words/wordlog"; 
+wd="$(ls -t1 $wf|grep -e '.log$'|sed -e 's/\.log//g')"; 
 ####
 lina() { printf %b "\e[0;2m"; for i in $(seq ${1:-15}); do printf %b "-"; done; printf %b "\e[0m"; }; 
 ####
@@ -10,16 +11,17 @@ lina() { printf %b "\e[0;2m"; for i in $(seq ${1:-15}); do printf %b "-"; done; 
 gum style --border hidden --padding "1 2 0 2" --margin "2 2 0 2" --align center \
 "$(printf %b "\e[38;2;211;44;162m"; sed -n 1p ${wf}/${wd}.log|tr -d "\n"; printf %b "\e[0m\n"; 
 lina 9)" | \
-ansifilter -M -s 60 -F "Cutive Mono" | tee "${wf}/${wd}.html"; 
+ansifilter -M -s 60 -F "Cutive Mono" > "${wf}/${wd}.html"; 
 ####
 (
 printf %b "\e[1;38;2;244;122;166m"; 
 sed -n 2p "${wf}/${wd}.log"; printf %b "\e[0m";
 sed -n 3p "${wf}/${wd}.log"|bat -ppfljs --theme TwoDark; 
 sed -n 4p "${wf}/${wd}.log"|fmt --goal 29|gum style --border hidden --padding "1 2 1 2" --margin "0 2 5 2" --background "#dcdcdc") | \
-ansifilter -M -F "Cutive Mono" -s 50 | tee -a "${wf}/${wd}.html"; 
+ansifilter -M -F "Cutive Mono" -s 50 >> "${wf}/${wd}.html"; 
 ####
 ####
-convert -background "#f4f8fa" -gravity center pango:"$(cat ${wf}/${wd}.html)" ${wf}/${wd}.jpg;
-# DejaVu Sans Mono
-# Noto Sans Mono
+convert -background "#f4f8fa" -gravity center pango:"$(cat ${wf}/${wd}.html)" ${wf}/${wd}.jpg; 
+}; 
+
+word22jpg; 
--- i/words/wordget_miriam_webster.sh
+++ i/words/wordget_miriam_webster.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 wordget_miriam() { 
 ##
-td="$HOME/logs/words/"; mkdir -p -m 775 $td/miriam 2>/dev/null; 
-tf="word_merriam_webster_$(date +%y%m%d_%H%M%S).log"; 
+mkt="$(mktemp)"; 
+td="$HOME/logs/words/wordlog"; 
+unset -v words; 
+mkdir -p -m 775 $td 2>/dev/null; 
+# tf="word_merriam_webster_$(date +%y%m%d_%H%M%S).log"; 
 ##
-w3m -dump https://www.merriam-webster.com/word-of-the-day > $td/$tf
+w3m -dump https://www.merriam-webster.com/word-of-the-day > "${mkt}"; 
+# $td/$tf
 #s
-local IFS='%'; words=($(cat $td/$tf | grep -e 'Est. 1828' -A20|tail -n+7|grep -vE 'play|Prev Next|━|What It Means|// '|sed 2,4"s/\ /\n/"|tr -s "\n" "\n- "|sed 1,3"a\\\n%\\n"|tr -d "\n")); local IFS=$'\n\t '; 
+# local IFS='%'; 
+local IFS=$'\n'; 
+words=($(cat "$mkt" | grep -e 'Est. 1828' -A20|tail -n+7|grep -vE 'play|Prev Next|━|What It Means'|sed 2,4"s/\ /\n\n/"|fmt -w999|grep -v '// '|tr -s "\n" "\n")); 
+
 ##
-printf %b "${words[*]}" | tee $td/miriam/$words.log; 
+printf %b "${words[0]}\n[ ${words[2]} ]\n\n${words[1]}\n${words[-1]}" > $td/$words.log; 
 }; 
 ##
 wordget_miriam; 
+
+
+# words=($(cat "$mkt" | grep -e 'Est. 1828' -A20|tail -n+7|grep -vE 'play|Prev Next|━|What It Means'|sed 2,4"s/\ /\n/"|sed "a\\\n%\\n"|tr -s " " " "|fmt -w 999|grep -v '// '|tr -d "\n")); 
