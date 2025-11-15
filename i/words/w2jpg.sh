5a8fbee572ce44ad8e80651dc6251046be5e6d49
aa <aa@aeniks.com>
2025-11-15 11:27:12 +0100

    aa_ASUSTeK_Sat Nov 15 11:27:12 AM CET 2025

--- i/words/w2jpg.sh
+++ i/words/w2jpg.sh
@@ -22,7 +22,7 @@ gum style --margin "0 2 0 2" --align center --border none \
 gum style --margin "0 2 0 2" --align center --border none --foreground "#888888" \
 "$(sed -n 3p $ww)"|ansifilter -M -F "monospace" -s 60 >> $wordlog/$word.htm; 
 
-gum style --margin "1 2 2 2" --border normal --padding "1 2 1 2" --border-foreground "#666666" \
+gum style --margin "0 2 2 2" --border hidden --padding "1 2 1 2" --border-foreground "#666666" \
 "$(sed -n 4,8p $ww|fmt -w 55 -g 40|bat -ppflc++ --theme Coldark-Cold)"|ansifilter -M -F "monospace" -s 60 >> $wordlog/$word.htm; 
 # gum style --margin "0 2 0 2" --align center --border none --foreground "#bbbbbb" "$(printf %b "${words[2]}")"|ansifilter -M -F "sans-serif" -s 40 >> $wordlog/$word.htm; 
 
