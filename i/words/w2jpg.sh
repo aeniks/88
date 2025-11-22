f97566649b6ec855e1bd04751dab9b3e255e1a77
aa <aa@aeniks.com>
2025-11-22 12:28:00 +0100

    aa_ASUSTeK_Sat Nov 22 12:28:00 PM CET 2025

--- i/words/w2jpg.sh
+++ i/words/w2jpg.sh
@@ -5,17 +5,16 @@ latest="$(command ls -t1 $wordlog|grep -e '.log$'|head -n1)";
 ww="$wordlog/$latest"; 
 [ "$1" ] && latest="${1/*\//}"; 
 [ "$1" ] && ww="$1"; 
-word="${latest/.*/}"; local IFS=$'\n\t '; 
+word="${latest/.*/}"; 
+local IFS=$'\n\t '; 
 ####
-
-
 # gum style --margin "0 2 0 2" --align center --border none "$(sed -n 1p $ww)"
 ## create file and spacing on top
 printf %b "  "| ansifilter -M -F "serif" -s 150 > $wordlog/$word.htm; 
-sed -n 1p $ww|ansifilter -M -F "serif" -s 260 >> $wordlog/$word.htm; 
+printf %b "  $(sed -n 1p $ww)  "|ansifilter -M -F "serif" -s 260 >> $wordlog/$word.htm; 
 sed -n 3p $ww|bat -ppfld --theme GitHub|ansifilter -M -F "code" -s 60 >> $wordlog/$word.htm; 
 sed -n 2p $ww|bat -ppfljs --theme Coldark-Cold|ansifilter -M -F "sans-serif" -s 60 >> $wordlog/$word.htm; 
-sed -n 4,8p $ww|fmt -w 48 -g 29|gum style --padding "1 5 2 5" --margin "0 1 2 0"|ansifilter -M -F "monospace" -s 60 >> $wordlog/$word.htm; 
+sed -n 4,6p $ww|fmt -w 48 -g 29|gum style --padding "1 5 5 5" --margin "0 1 0 0"|ansifilter -M -F "monospace" -s 60 >> $wordlog/$word.htm; 
 ############
 # gum style --margin "0 2 2 2" --border hidden --padding "1 2 1 2" --border-foreground "#666666" "$(
 # printf %b "  "|ansifilter -M -F "serif" -s 20 >> $wordlog/$word.htm; 
@@ -38,6 +37,7 @@ convert -border ${imh}x$((imw / 2 "+ 122")) -bordercolor "$hexx" "$wordimg/${wor
 ####
 cp "$wordimg/${word}.jpg" -t "$HOME/fofo/wordimg"; 
 mv "$wordlog/${word}.htm" "$wordimg/${word}2.jpg" -t ~/tmp; 
+mv "$wordlog/${word}.log" -t $HOME/logs/words/up
 ####
 }; 
 words2jpg; 
