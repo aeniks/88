9db2a1a377c5bd1276114c80591dfb3708f2228c
u0_a79 <u0_a79@localhost>
2025-11-22 02:00:23 +0100

    _Google_Sat Nov 22 02:00:23 CET 2025

--- f/mkd.sh
+++ f/mkd.sh
@@ -4,7 +4,7 @@ printf %b "\n\n\n\n\e[4A\n $c2 new folder?$re  ";
 read -ei "$PWD/$1" "folder";
 mkdir -p $folder -m 775;
 # chown "$SUDO_USER":"$USER" "$folder";
-cd $folder; printf %b "\n\t $cyan$bold> $pink$PWD$cyan <$re\n\t * * * *\n"; ls -caklhuptr --group-d
-irectories-first;
+cd $folder; printf %b "\n\t $cyan$bold> $pink$PWD$cyan <$re\n\t * * * *\n"; 
+ls -caklhuptr --group-directories-first; 
 }; 
 
