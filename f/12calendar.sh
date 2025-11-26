bf6687b10adc5635c3fb75b81780d452744f9c57
aa <aa@aeniks.com>
2025-11-26 23:25:09 +0100

    aa_ASUSTeK_Wed Nov 26 11:25:09 PM CET 2025

--- f/12calendar.sh
+++ f/12calendar.sh
@@ -18,7 +18,7 @@ printf %b " $(date +%a\ %b\ %d\ %Y\ \|\ %T)\n"|bat -ppfljava --theme DarkNeon;
 12calendarget() { 
 
 dott; printf %b "  getting cal ·· "; 
-(curl -sL "https://script.google.com/macros/s/AKfycbye8O0u3we9g5Xt3HilbKzLdjlC94OwSj7QPprmzc0pWI5dZ_ORE5YSaFmlyCJ-JqBQ/exec" && printf %b "\nEPOCH_${EPOCHSECONDS}") > $HOME/logs/calendar.json && printf %b "\e[G"; 
+(curl -sL "https://script.google.com/macros/s/AKfycbye8O0u3we9g5Xt3HilbKzLdjlC94OwSj7QPprmzc0pWI5dZ_ORE5YSaFmlyCJ-JqBQ/exec" && printf %b "\nEPOCH_${EPOCHSECONDS}") > $HOME/logs/calendar.json && printf %b "\e[G\e[K"; 
 ####
 ( cat $HOME/logs/calendar.json | tr -s "," "\n" | grep -vE 'EPOCH_|h_|description|end_date' | sed '/start_date_time/{s/.[0-9]*[-T]//g}' | cut -f1 -d "+" | sed -e "s/{\"summary/€\n/g"| cut -f 2- -d":" | tr -s "\n€[]" " \n"|sed -e 's/\ \"//g' -e 's/\"\ /\"/g' | col -xb | column --separator "\"" --table --output-separator " | " --table-columns "1234567890123456" --table-right 1 | tail -n+2 | bat -ppflr --theme Visual\ Studio\ Dark+ ) > $HOME/logs/cal.log; 
 ####
