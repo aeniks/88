abbe393dedf0f3607d3ad557cde562f3c84cb87d
u0_a26 <u0_a26@localhost>
2025-11-27 11:56:16 +0100

    _Xiaomi_Thu Nov 27 11:56:16 CET 2025

--- install/install_tmux_conf.sh
+++ install/install_tmux_conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## install tmux
 printf %b "\n-- installing tmux & configs ... \n"; 
-mkdir -m 775 -p $HOME/.config/tmux/tpm/plugins 2>/dev/null; 
+mkdir -m 775 -p $HOME/.config/tmux/plugins/tpm 2>/dev/null; 
 ln -s $HOME/88/c/tmux/tmux.conf -t $HOME/.config/tmux 2>/dev/null; 
 ln -s $HOME/88/c/tmux/plugins/* -t $HOME/.config/tmux/plugins 2>/dev/null; 
 ln -s $HOME/88/c/tmux/tmuxcompletions.sh -t $HOME/.config/tmux 2>/dev/null; 
