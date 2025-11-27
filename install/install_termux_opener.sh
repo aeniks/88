ef896c08471c320bf3a09c684523798e7e1435f0
u0_a79 <u0_a79@localhost>
2025-11-27 14:16:36 +0100

    _Google_Thu Nov 27 14:16:36 CET 2025

--- install/install_termux_opener.sh
+++ install/install_termux_opener.sh
@@ -4,3 +4,4 @@ apt install -y ffmpeg;
 mkdir $HOME/bin 2>/dev/null; 
 ln -s $HOME/88/c/termux-url-opener -t $PREFIX/bin; 
 ln -s $HOME/88/c/termux-url-opener $PREFIX/bin/termux-file-editor; 
+printf %b "\n-- installed termux-url-opener!\n"; 
--- install/install_tmux_conf.sh
+++ install/install_tmux_conf.sh
@@ -2,7 +2,6 @@
 ## install tmux
 printf %b "\n-- installing tmux & configs ... \n"; 
 mkdir -m 775 -p $HOME/.config/tmux/plugins/tpm 2>/dev/null; 
-ln -s $HOME/88/c/tmux/tmux.conf -t $HOME/.config/tmux 2>/dev/null; 
+ln -s ""$HOME/88/c/tmux/{hl.tmux,tmux.conf,tmuxbg.sh,tmuxcompletions.sh}" " -t $HOME/.config/tmux 2>/dev/null; 
 ln -s $HOME/88/c/tmux/plugins/* -t $HOME/.config/tmux/plugins 2>/dev/null; 
-ln -s $HOME/88/c/tmux/tmuxcompletions.sh -t $HOME/.config/tmux 2>/dev/null; 
 printf %b "\n-- done!\n\n"; 
