6fe57c8216d9bef08747e63ef894a61059df4e24
u0_a79 <u0_a79@localhost>
2025-11-27 11:45:50 +0100

    _Google_Thu Nov 27 11:45:50 CET 2025

---
+++ install/install_termux_opener.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+## install termux opener
+apt install -y ffmpeg; 
+mkdir $HOME/bin 2>/dev/null; 
+ln -s $HOME/88/c/termux-url-opener -t $PREFIX/bin; 
+ln -s $HOME/88/c/termux-url-opener -t $PREFIX/bin/termux-file-editor; 
