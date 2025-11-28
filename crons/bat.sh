1e269063dd7721a93c685a958919aa3e28f19e30
u0_a79 <u0_a79@localhost>
2025-11-28 19:31:44 +0100

    _Google_Fri Nov 28 19:31:44 CET 2025

--- crons/bat.sh
+++ crons/bat.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-sleep 1; termux-battery-status > "$HOME/logs/b/battery.log"; 
-
-bat="$(cat ~/logs/b/battery.log |grep -e "percentage"|tr -d 'A-z ,\":' | tee ~/logs/b/bp.log)"; 
-
-bcolor="$(printf %b "$(($(cat ~/logs/b/bp.log)/10*4+124-4))" | tee ~/logs/b/bcolor.log)"; 
-###
-bcharge='\e[1;92m'; cat $HOME/logs/b/battery.log|grep -qe "DISCHARGING" 2>/dev/null && bcharge='\e[0;97m'; 
+sleep 1; termux-battery-status > ~/logs/b/battery.log; 
+cat ~/logs/b/battery.log|grep -e "percentage"|tr -d 'A-z ,\":' > ~/logs/b/bp.log; 
 ####
-printf %b '${bcharge}[\e[0m\e[${bcolor}m${bat}${bcharge}]\e[0m ' > $HOME/logs/b/bat.log; 
+# bat="$(
+####
+# bcolor="$(($(cat ~/logs/b/bp.log)/10*4+124-4)) | tee ~/logs/b/bcolor.log)"; 
+###
+# bcharge='\e[1;92m'; cat $HOME/logs/b/battery.log|grep -qe "DISCHARGING" 2>/dev/null && bcharge='\e[0;97m'; 
+####
+# printf %b '${bcharge}[\e[0m\e[${bcolor}m${bat}${bcharge}]\e[0m ' > $HOME/logs/b/bat.log; 
