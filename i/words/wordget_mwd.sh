f7297a278f319d01e6e98f4d68e0af568edc52ca
aa <aa@aeniks.com>
2025-11-29 08:29:26 +0100

    aa_ASUSTeK_Sat Nov 29 08:29:26 AM CET 2025

--- i/words/wordget_mwd.sh
+++ i/words/wordget_mwd.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 ## word of the day merriam webster 
 wordget_mw_com() { 
-sh $HOME/88/i/words/wpre.sh; 
-local IFS=$'\n'; 
-fold="$HOME/logs/words/wordlog"; 
-date=$(date +%F-%H_%M_%S); echo; echo; 
-wlog="$HOME/logs/words/tmp/mw_com_$date"; 
 ########
-lynx -width 800 -nonumbers -nomargins -dump https://www.merriam-webster.com/word-of-the-day | \
+[ "$1" ] && wordget="dictionary/$@" || wordget="word-of-the-day"; 
+sh $HOME/88/i/words/wpre.sh; local IFS=$'\n'; fold="$HOME/logs/words/wordlog"; date=$(date +%F-%H_%M_%S); wlog="$HOME/logs/words/tmp/mw_com_$date"; 
+########
+lynx -width 800 -nonumbers -nomargins -dump \
+"https://www.merriam-webster.com/$wordget" | \
 sed 's/_*//g' | \
 grep -e 'Est. 1828' -m1 -A22 | \
 sed -e "/\\/\\//,/*/d" -e 1,5d | \
 grep -vE 'What It Means|Prev Next' | \
 sed -e 4d -e '5y/\ /\n/' | \
 tr -s "\n " "\n " | \
-sed -e 's/^[ \t]*//' -e '/^$/d' > $wlog.log;
+sed -e 's/^[ \t]*//' -e '/^$/d' | \
+sed -e '3s/^/[/' -e '3s/$/]/' > $wlog.log;
 ########
-word="$(head -n1 $wlog.log)"; 
-cat $wlog.log | sed -e '3s/^/[/' -e '3s/$/]/' > $fold/$word.log; 
+word="$(head -n1 $wlog.log|tee $fold/$word.log)"; 
+printf %b "\n----\n$word\n"; 
+sed -n 3p $wlog.log | tee -a $fold/word.log; 
+sed -n 2p $wlog.log | tee -a $fold/word.log; 
+sed -n 4,5p $wlog.log | tee -a $fold/word.log; 
+printf %b "----\n"; 
 ########
-printf %b "\n----\n$word\n----\n"; 
 ########
-
 }; 
 ########
 wordget_mw_com; 
-
