ba3cd837856fb01f483400ec47fc66e161b3e49b
u0_a26 <u0_a26@localhost>
2025-11-29 04:03:56 +0100

    _Xiaomi_Sat Nov 29 04:03:56 CET 2025

--- install.sh
+++ install.sh
@@ -1,129 +1,76 @@
 #!/bin/bash
 ## installer 
 install88() {
-hash sudo 2>/dev/null && sudo="sudo"; hash sudo 2>/dev/null || alias sudo=' ';
-[ "$PREFIX" ] && unset sudo; [ $UID = 0 ] && unset sudo && unalias sudo; 
-mkdir $HOME/{tmp,logs,gh,backup,.config} 2>/dev/null; 
-export tmp=$HOME/tmp time=$(date +%y%m%d%H%m%S) bu=$HOME/backup; 
+hash sudo 2>/dev/null && sudo="sudo"; hash sudo 2>/dev/null || alias sudo=' '; [ "$PREFIX" ] && unset sudo; [ $UID = 0 ] && unset sudo && unalias sudo; mkdir $HOME/{tmp,logs,gh,backup,.config} 2>/dev/null; export tmp=$HOME/tmp time=$(date +%y%m%d%H%m%S) bu=$HOME/backup; for i in ""pp{0..8}" "; do unset -v $i; done; unset -v _yno_prompt ny p pp yy 2>/dev/null; 
+local IFS=$' \n\t' start=$HOME/88 green='\e[32m' dim='\e[2m' re='\e[0m' red='\e[31m' cyan='\e[36m' yellow='\e[33m' blue='\e[36m' bold='\e[1m' height="$(stty size|cut -f1 -d" ")" width="$(stty size|cut -f2 -d" ")" yno='\e[0m[\e[2mY\e[0m/\e[2mn\e[0m]' c2='    \e[0m\e[36m--\e[0m' uu="60" enter='\e[\e[0m [\e[2mENTER\e[0m] to accept or [\e[2mq\e[0m]\e[2mto Quit \e[0m' x="2>/dev/null" yy="1"; 
 #####
-local IFS=$' \n\t' start=$HOME/88 green='\e[32m' dim='\e[2m' re='\e[0m' red='\e[31m' cyan='\e[36m' yellow='\e[33m' blue='\e[36m' bold='\e[1m' height="$(stty size|cut -f1 -d" ")" width="$(stty size|cut -f2 -d" ")" yno='\e[0m[\e[2mY\e[0m/\e[2mn\e[0m]' c2='\e[0m\e[36m--\e[0m' uu="60" enter='\e[\e[0m [\e[2mENTER\e[0m] to accept or [\e[2mq\e[0m]\e[2mto Quit \e[0m' x="2>/dev/null"; 
-unset _yno_prompt ny 2>/dev/null; 
-for i in ""p{1..8}" "; do unset -v $i; done; 
 #####
-# unalias p1 p2 2>/dev/null; 
-# p1() { p2=" ${@}"; for i in $(seq ${#p2}); do sleep .04; printf %b "${p2:${i}:1}"; done; }; p2() { printf %b "$@"; }; 
-####
-_quit() { [[ "$ny" = "q" ]] && printf %b "" && return 2; }; 
-####
-p1() { p2=" ${@}"; for i in $(seq ${#p2}); do sleep .02; printf %b "${p2:${i}:1}"; done; }; ## rolling text 
-p2() { printf %b "$@"; }; 
-####
-_link() { ln -s $@ 2>/dev/null; }; 
-########
-_newcolor() { printf %b "\e[38;5;$((uu++))m"; sleep .02; }; 
-########
-_backup() { mv -fb --suffix="$time" $@ -t $bu &>/dev/null; }; 
-##
+p1() { p2=" ${@}"; for i in $(seq ${#p2}); do sleep .04; printf %b "${p2:${i}:1}"; done; }; p2() { printf %b "$@"; }; 
+for i in $(seq $((height))); do printf %b "\n"; done; printf %b "\e[H\n\n       "; p1 "hello. "; sleep 2; printf %b "\n\n"; 
+#####
+#####
+#####
+p1() { p2=" ${@}"; for i in $(seq ${#p2}); do sleep .04; printf %b "${p2:${i}:1}"; done; }; p2() { printf %b "$@"; }; 
+#####
 _ok() { printf %b "\e[40G     \e[8D  "; p2 "\e[0;1m [\e[0;92m"; p1 "OK"; p2 "\e[0;1m]  \e[0m\n"; }; 
-_yno() { 
-yno='\e[0m[\e[2mY\e[0m/\e[2mn\e[0m]'; 
-[ "$1" ] && ny=${1}; 
-printf %b "\e[?25h\e[40G\b\b\b\b\b\b\b\b$yno "; 
-printf -v _yno_${1} "false"; read -rsn1 ny; 
-[[ -z $ny || $ny = y ]] && printf -v "p$((yy++))" %b "ok" && printf -v _yno_${1} "true"; _ok;  
-[[ $ny = q ]] && printf %b "\e[?25h" && printf %b "\n\n" && return 1; 
-# [[ $ny = q ]] && printf %b "\e[?25h" && _quit && return 2; 
-}; 
-########
-for i in $(seq $((height))); do printf %b "\n"; done; printf %b "\e[H\n\n"; 
-# for i in $(seq $((height / 2))); do printf %b "\e[38;5;$((RANDOM%16 + 111))m$i\n"; sleep .01; done; for i in $(seq $((height / 2 - 5))); do printf %b "\e[K\e[A\e[2K"; sleep .02; done; printf %b "\e[?25l"; 
-ee=1; 
-_prompt() { [[ "$ny" = "q" ]] && return 1; p2 "    $c2 "; p1 "$@"; _yno prompt; 
-if [[ $_yno_prompt == true ]]; then _newcolor; else return 1; fi; }; 
+##
+_yno() { yno='\e[0m[\e[2mY\e[0m/\e[2mn\e[0m]'; [ "$1" ] && ny=${1}; printf %b "\e[?25h\e[40G\b\b\b\b\b\b\b\b$yno "; printf -v _yno_${1} "false"; read -rsn1 ny; [[ -z $ny || $ny = y ]] && printf -v "pp$((yy++))" %b "ok" && printf -v _yno_${1} "true"; _ok; [[ $ny = q ]] && printf %b "\e[?25l\n\n" && return 1; }; 
+##
+_link() { ln -s $@ 2>/dev/null; }; 
+_backup() { mv -fb --suffix="$time" $@ -t $bu &>/dev/null; }; 
+_newcolor() { printf %b "\e[38;5;$((uu++))m"; sleep .02; }; 
+_prompt() { [[ "$ny" = "q" ]] && return 1; p2 "$c2 "; p1 "$@"; _yno prompt; if [[ $_yno_prompt == true ]]; then _newcolor; else return 1; fi; }; 
+#####
+#####
+########################################
+########################################
 ####
 _prompt "update system? "; 
 ####
-
 _prompt "install basic apps? "; 
-
 ####
-_prompt "download config-files? "; 
+_prompt "download config-files? " && (p2 "$c2 "; p1 "where to? "; read -ei "$HOME/" "hstart"; start="${hstart}/88"; sleep .2; export start="${start/\/\///}"); 
 ####
-_prompt "install config-files? " && (
+_prompt "install config-files? "; 
 ####
-_backup $HOME/.inputrc; _newcolor; _link $start/c/inputrc $HOME/.inputrc; _newcolor; 
 ####
-confolders=($(ls -1p $HOME/88/c|grep "/")); 
-# conf=(micro newsboat bat lf tmux htop glow aichat ranger); 
-for q in ${confolders[*]}; do 
-mkdir -p $HOME/.config/$q 2>/dev/null; 
-_backup $HOME/.config/$q/*; _newcolor; 
-ln -s $start/c/$q/* -t $HOME/.config/$q/ 2>/dev/null; sleep .2; 
-printf %b "\n\e[0m"; p1 "updated"; _newcolor; printf %b " $q"; 
-done; _newcolor; 
-printf %b "${PATH}:${HOME}/.local/bin" > $HOME/.config/path.sh; chmod 775 $HOME/.config/path.sh; _newcolor; 
+_prompt "login to github ? "; 
+########################################
+########################################
 ####
-printf %b "\n\e[0m"; 
-p1 'added PATH to ~/.config/path.sh '; 
-printf %b "\n\e[0m"; 
-_newcolor; 
-##########
-); 
 ####
-_prompt "login to github ? " && (
+[ "$ny" = "q" ] && echo && return 0; 
 ####
-ghuser="$(id -nu)"; ghmail="$(id -nu)@$(hostname)"; gh_aeniks="$start/c/gpg/gh_aeniks.gpg"; 
-####
-for ss in {ssh,openssl,openssh-server,gh,git} ; do hash $ss && echo $ss && break; _newcolor; $sudo apt install -y ${ss}; done; 
-####
-printf %b "\n\n\e[A\e[96m -- \e[0m"; 
-gpg --pinentry-mode loopback -o "gh.txt" -d "$gh_aeniks"; 
-gh auth login --with-token < "gh.txt"; printf "$c2 "; rm gh.txt; sleep .2;
-gh auth status && printf %b "\e[60G      \e[8D  "; p2 "\e[0;1m [\e[0;92m"; p1 "OK"; p2 "\e[0;1m]  \e[0m\n"; 
-git config --global user.name $ghuser; git config --global user.email $ghmail; git config --global init.defaultBranch main; 
-####
-[ $(ls $HOME/.ssh/*.pub) ] || [ -r $HOME/.ssh/id_ed25519.pub ] || ssh-keygen -N '' -f $HOME/.ssh/id_ed25519; chmod 600 $HOME/.ssh/*; chmod 644 $HOME/.ssh/*.pub;
-####
-gh config set git_protocol ssh; gh ssh-key add $HOME/.ssh/id_ed25519.pub; 
-printf %b "\e[96m\u990 \e[0m"; ssh -T git@github.com; printf %b "\n"; 
-####
-cd $start; git config remote.origin.url git@github.com:aeniks/88.git 2>/dev/null; cd -; 
-####
-); 
-[ "$ny" = "q" ] && return 0; 
 echo; 
 echo; 
-
-if [ $p1 ]; then 
-# if [ "$_yno_prompt1" = "true" ]; then 
-$sudo apt update && $sudo apt upgrade -y; 
+if [ $pp1 ]; then $sudo apt update && $sudo apt upgrade -y; fi; 
+echo; 
+echo; 
+if [ $pp2 ]; then for i in $(ls -1 $HOME/88/install/ap); do rr="$((RANDOM%222))";  printf %b "\ec\e[0m -- \e[48;5;${rr}m ${i} \e[0m --\n\e[38;5;${rr}m "; command -v $i &>/dev/null || $sudo apt install -y $i; done; printf %b "\e[2J\e[2H\n\n\e[0m -- \e[48;5;196m DONE \e[0m --\n\n"; fi; 
+echo; 
+echo; 
+if [ $pp3 ]; then _backup "$start"; _newcolor; hash -v git &>/dev/null || $sudo apt install -y git 2>/dev/null; git clone https://github.com/aeniks/88.git $start 2>/dev/null; cd "$start"; git config remote.origin.url git@github.com:aeniks/88.git; cd $OLDPWD; 
 fi; 
 echo; 
 echo; 
-if [ $p2 ]; then for i in $(ls -1 $HOME/88/install/ap); do rr="$((RANDOM%222))";  printf %b "\ec\e[0m -- \e[48;5;${rr}m ${i} \e[0m --\n\e[38;5;${rr}m "; command -v $i &>/dev/null || $sudo apt install -y $i; done; printf %b "\e[2J\e[2H\n\n\e[0m -- \e[48;5;196m DONE \e[0m --\n\n"; fi; 
+if [ $pp4 ]; then _backup $HOME/.inputrc; _newcolor; _link $start/c/inputrc $HOME/.inputrc; _newcolor; confolders=($(ls -1p $HOME/88/c|grep "/")); for q in ${confolders[*]}; do mkdir -p $HOME/.config/$q 2>/dev/null; _backup $HOME/.config/$q/*; _newcolor; ln -s $start/c/$q/* -t $HOME/.config/$q/ 2>/dev/null; sleep .2; printf %b "\n\e[0m"; p1 "updated"; _newcolor; printf %b " $q"; done; _newcolor; printf %b "${PATH}:${HOME}/.local/bin" > $HOME/.config/path.sh; chmod 775 $HOME/.config/path.sh; _newcolor; printf %b "\n\e[0m"; p1 'added PATH to ~/.config/path.sh '; printf %b "\n\e[0m"; _newcolor; fi;
 echo; 
 echo; 
-if [ $p3 ]; then p2 "    $c2 "; p1 "where to? "; read -ei "$HOME/" "hstart"; start="${hstart}/88"; sleep .2; export start="${start/\/\///}"; _backup "$start"; _newcolor; hash -v git &>/dev/null || $sudo apt install -y git 2>/dev/null; git clone https://github.com/aeniks/88.git $start 2>/dev/null; cd "$start"; git config remote.origin.url git@github.com:aeniks/88.git; cd $OLDPWD; fi; 
+if [ $pp5 ]; then ghuser="$(id -nu)"; ghmail="$(id -nu)@$(hostname)"; gh_aeniks="$start/c/gpg/gh_aeniks.gpg"; for ss in {ssh,openssl,openssh-server,gh,git} ; do hash $ss && echo $ss && break; _newcolor; $sudo apt install -y ${ss}; done; printf %b "\n\n\e[A\e[96m -- \e[0m"; gpg --pinentry-mode loopback -o "gh.txt" -d "$gh_aeniks"; gh auth login --with-token < "gh.txt"; printf "$c2 "; rm gh.txt; sleep .2; gh auth status && printf %b "\e[60G      \e[8D  "; p2 "\e[0;1m [\e[0;92m"; p1 "OK"; p2 "\e[0;1m]  \e[0m\n"; git config --global user.name $ghuser; git config --global user.email $ghmail; git config --global init.defaultBranch main; [ $(ls $HOME/.ssh/*.pub) ] || [ -r $HOME/.ssh/id_ed25519.pub ] || ssh-keygen -N '' -f $HOME/.ssh/id_ed25519; chmod 600 $HOME/.ssh/*; chmod 644 $HOME/.ssh/*.pub; gh config set git_protocol ssh; gh ssh-key add $HOME/.ssh/id_ed25519.pub; printf %b "\e[96m\u990 \e[0m"; ssh -T git@github.com; printf %b "\n"; cd $start; git config remote.origin.url git@github.com:aeniks/88.git 2>/dev/null; cd $OLDPWD; fi;
 echo; 
 echo; 
-
-# if [ $p4 ]; then 	fi;
-# if [ $p5 ]; then 	fi;
-# if [ $p6 ]; then 	fi;
-
 }; 
+####
+####
+
 
 install88; 
-#!/bin/bash
-## install tmux
 
-# file
-# mediainfo
-# bat
-# batcat
-# ncdu
 
-####
-## apt install -y autoconf libevent ncurses pkg-config automake; 
-## mkdir $HOME/gh 2>/dev/null; cd $HOME/gh; gh repo clone tmux/tmux
-####
+
+
+##
+## for i in $(seq $((height / 2))); do printf %b "\e[38;5;$((RANDOM%16 + 111))m$i\n"; sleep .01; done; for i in $(seq $((height / 2 - 5))); do printf %b "\e[K\e[A\e[2K"; sleep .02; done; printf %b "\e[?25l"; 
+## unalias p1 p2 2>/dev/null; p1() { p2=" ${@}"; for i in $(seq ${#p2}); do sleep .04; printf %b "${p2:${i}:1}"; done; }; p2() { printf %b "$@"; }; 
+##
+##
